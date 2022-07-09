5d53473fbeded94765f2dd968636240e7b2ef295
Suryansh Makharia <makhariasuryansh7@gmail.com>
2022-07-10 01:41:33 +0530

    add icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,10 +5,7 @@ export default function Document() {
     <Html className="scroll-smooth">
       <Head>
         <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-        <link rel="icon" type="image/svg+xml" href="/favicons/favicon.svg" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16.png" />
-        <link rel="manifest" href="/favicons/site.webmanifest" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon.png" />
         <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
