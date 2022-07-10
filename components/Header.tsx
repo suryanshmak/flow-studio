aeb0046809e59196767df033ae1e5cc1dcd006ad
Suryansh Makharia <makhariasuryansh7@gmail.com>
2022-07-10 22:21:15 +0530

    add hamburger menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,28 +1,53 @@
 import Image from "next/image"
 import Link from "next/link"
+import { useState } from "react"
+import { FaTwitter } from "react-icons/fa";
 
 export const Header = () => {
+  const [active, setActive] = useState<boolean>(false);
+
   return (
-    <header className="w-full absolute top-0 px-12 py-4 flex items-center gap-[6ch]">
-      <Link href="/">
-        <a>
-          <Image src="/header-img.svg" alt="" height={52} width={52} />
-        </a>
-      </Link>
-      <nav className="hidden sm:flex items-center space-x-8 grow">
-        <Link href='/#home'>
-          <a className="hover:text-neutral-200">Home</a>
+    <>
+      <header className="w-full absolute top-0 px-12 py-4 flex items-center justify-between sm:justify-stretch sm:gap-[6ch]">
+        <Link href="/">
+          <a><Image src="/header-img.svg" alt="" height={52} width={52} /></a>
         </Link>
-        <Link href='/#work'>
-          <a className="hover:text-neutral-200">Work</a>
-        </Link>
-        <Link href='/#team'>
-          <a className="hover:text-neutral-200">Team</a>
-        </Link>
-      </nav>
-      <Link href="/#contact">
-        <a className="hidden sm:block border-btn duration-400">Contact</a>
-      </Link>
-    </header>
+        <nav className="hidden sm:flex items-center justify-between grow">
+          <p className="space-x-8">
+            <LinkTo href='/#home' text="Home" />
+            <LinkTo href='/#work' text="Work" />
+            <LinkTo href='/#team' text="Team" />
+          </p>
+          <LinkTo href="/#contact" text="Contact" {...{ className: "border-btn duration-400" }} />
+        </nav>
+        <button className={`sm:hidden flex flex-col items-center z-50 ${active ? 'justify-center' : 'space-y-2'}`} onClick={() => setActive(prev => !prev)}>
+          <div className={`burger-line ${active && 'rotate-45 duration-400'}`} />
+          <div className={`burger-line ${active && 'hidden opacity-0 duration-400'}`} />
+          <div className={`burger-line ${active && '-rotate-45 duration-400'}`} />
+        </button>
+      </header>
+      {active ?
+        (
+          <div className="sm:hidden fixed top-0 left-0 bg-[#000122] z-10 w-screen h-screen py-[12ch] flex flex-col text-center justify-between" onClick={() => setActive(prev => !prev)}>
+            <nav className="flex flex-col items-center space-y-6">
+              <LinkTo href='/#home' text="Home" />
+              <LinkTo href='/#work' text="Work" />
+              <LinkTo href='/#team' text="Team" />
+              <LinkTo href="/#contact" text="Contact" {...{ className: "border-btn duration-400" }} />
+            </nav>
+            <div className="w-full flex justify-center">
+              <Link href="https://twitter.com/flowstudio_">
+                <a><FaTwitter className="text-[3ch]" /></a>
+              </Link>
+            </div>
+          </div>
+        ) : null}
+    </>
   )
 }
+
+const LinkTo = ({ href, text, ...props }: { href: string, text: string }) => (
+  <Link href={href}>
+    <a className="hover:text-neutral-200" {...props}>{text}</a>
+  </Link >
+)
--- sections/Work.tsx
+++ sections/Work.tsx
@@ -1,25 +1,29 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 
 export const Work = () => {
   const workImgs = ["/work/img-1.png", "/work/img-2.png", "/work/img-3.jpg", "/work/img-4.jpg"];
   const ref = useRef<HTMLDivElement>(null);
 
-  const onWheel = (e: React.WheelEvent) => {
-    e.preventDefault();
-    document.firstElementChild?.classList.add('locked__')
+  useEffect(() => {
+    const onWheel = (e: React.WheelEvent) => {
+      e.preventDefault();
 
-    if (e.deltaX) return;
-    if (e.deltaY !== 0) {
-      ref.current?.scrollTo({
-        left: e.deltaY > 0 ? (ref.current.scrollLeft + e.deltaY + 120) : (ref.current.scrollLeft + e.deltaY - 120),
-        behavior: "smooth"
-      });
-    }
-  };
+      if (e.deltaX) return;
+      if (e.deltaY !== 0) {
+        ref.current?.scrollTo({
+          left: e.deltaY > 0 ? (ref.current.scrollLeft + e.deltaY) : (ref.current.scrollLeft + e.deltaY - 120),
+          behavior: "smooth"
+        });
+      }
+    };
+
+    document.body.addEventListener("wheel", onWheel);
+    return () => document.body.removeEventListener("wheel", onWheel);
+  }, [])
 
   return (
     <div className="py-16">
-      <div className="px-[8ch] flex items-center space-x-[8ch] overflow-x-scroll no-scrollbar" onWheel={onWheel} ref={ref}>
+      <div className="px-[8ch] flex items-center space-x-[8ch] overflow-x-scroll no-scrollbar" ref={ref}>
         {workImgs.map((src, idx) => (
           <img key={`${idx}`} src={src} alt="" draggable={false} className="rounded-lg w-[360px] md:w-[420px] lg:w-[460px] xl:w-[520px]" />
         ))}
