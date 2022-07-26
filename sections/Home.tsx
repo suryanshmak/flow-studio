f01cffde940e531f9a95e6b94cf95a2b0c8488c9
Suryansh Makharia <makhariasuryansh7@gmail.com>
2022-07-26 16:40:20 +0000

    remove vid controls

--- sections/Home.tsx
+++ sections/Home.tsx
@@ -1,7 +1,11 @@
 import Link from 'next/link'
+import { useEffect, useRef } from 'react'
 
-export const Home = () => (
-  <div className="flex flex-col xl:flex-row">
+export const Home = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  return (
+   <div className="flex flex-col xl:flex-row">
     <div className="space-y-10 lg:basis-[70%]">
       <h1 className="header">
         Meet the New <span className="gradient-text">Digital</span> Age.
@@ -16,13 +20,12 @@ export const Home = () => (
         </Link>
       </div>
     </div>
-    <div className="w-full sm:w-[90%] md:w-[520px] lg:w-[640px] xl:w-[840px] h-[400px] grid place-items-center relative mr-4">
+    <div className="w-full sm:w-[90%] md:w-[520px] lg:w-[640px] xl:w-[840px] h-[400px] grid place-items-center relative mr-4 cursor-pointer">
       <video
         src="/flowstudio.mp4"
-        autoPlay
+        ref={videoRef}
+        onClick={() => videoRef.current?.paused ? videoRef.current.play() : videoRef.current?.pause()}
         loop
-        controls
-        playsInline
         className="z-[1] w-full h-full xl:mb-12"
       />
       <img
@@ -31,5 +34,6 @@ export const Home = () => (
         className="h-[640px] absolute right-24 lg:right-[-8ch] top-[-8ch] sm:top-[-4ch] xl:top-[-8ch]"
       />
     </div>
-  </div>
-)
+   </div>
+  )
+}
--- sections/Work.tsx
+++ sections/Work.tsx
@@ -2,13 +2,13 @@ import React, { useEffect, useRef } from "react";
 import { HorizontalScroll } from "../components/HorizontalScroll";
 
 export const Work = () => {
-  const workImgs = ["/work/img1.svg", "/work/img2.svg", "/work/img3.svg", "/work/img4.svg", "/work/img5.svg"];
+  const workImgs = ["/work/img1.png", "/work/img2.png", "/work/img3.png", "/work/img4.png", "/work/img5.png"];
 
   return (
     <div className="py-8">
       <HorizontalScroll>
         {workImgs.map((src, idx) => (
-          <img key={`${idx}`} src={src} alt="" draggable={false} className="rounded-sm w-[360px] h-[200px] md:w-[420px] md:h-[280px] lg:w-[460px] xl:w-[520px] lg:h-[360px] object-cover" />
+          <img key={`${idx}`} src={src} alt="" draggable={false} className="rounded-sm w-[360px] md:w-[420px] lg:w-[460px] xl:w-[520px] object-cover" />
         ))}
       </HorizontalScroll>
     </div>
